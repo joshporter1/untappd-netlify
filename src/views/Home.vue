9115cb578669a876daea43a65960ac1390433148
Josh Porter <joshporter1@gmail.com>
2019-02-20 20:54:57 -0500

    Added fetch call

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ users[0].name }}: {{ users[0].count }}</h1>
+    <h1>{{ users[1].name }}: {{ users[1].count }}</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      users: [
+        {
+          name: null,
+          count: null
+        },
+        {
+          name: null,
+          count: null
+        }
+      ]
+    };
+  },
+  watch: {
+    "$route.params": {
+      immediate: true,
+      handler(val) {
+        this.users[0].name = val.user1;
+        this.users[0].count = null;
+        this.users[1].name = val.user2;
+        this.users[1].count = null;
+        this.updateUsers();
+      }
+    }
+  },
+  computed: {
+    base() {
+      if (process.env.NODE_ENV === "development")
+        return "http://localhost:8010/proxy";
+      return "/proxy";
+    }
+  },
+  methods: {
+    updateUsers() {
+      this.users.map((user, i) => {
+        let url = `${this.base}/user/${user.name}`;
+        fetch(url)
+          .then(response => {
+            return response.text();
+          })
+          .then(html => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, "text/html");
+            let unique = doc.querySelector(".stats > a:nth-child(2) > span")
+              .innerText;
+            this.users[i].count = unique;
+          })
+          .catch(function(err) {
+            console.log("Failed to fetch page: ", err);
+          });
+      });
+    }
   }
 };
 </script>
